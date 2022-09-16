0d535656c39c738ba58deaba6ebd3c1262cf8880
Westindiess <gregoryalexiss@gmail.com>
2022-09-16 13:31:36 +0200

    create go back method

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../heroes/heroes.model';
+import { HeroService } from '../hero.service';
 
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
-import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -27,5 +26,9 @@ export class HeroDetailComponent implements OnInit {
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
